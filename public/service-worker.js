6cbcbe8f3bdae0e6e95ad4573a672e76f09465f6
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-12-19 09:32:44 -0600

    add / to files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,9 @@
 const FILES_TO_CACHE = [
+  "/",
   "./index.html",
   "./css/styles.css",
-  "./index.js",
+  "./js/index.js",
+  "./js/idb.js",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
@@ -23,7 +25,7 @@ self.addEventListener('install', function (e) {
           return cache.addAll(FILES_TO_CACHE)
         })
       )
-})
+});
 
 self.addEventListener('activate', function(e) {
     e.waitUntil(
@@ -58,4 +60,4 @@ self.addEventListener('activate', function(e) {
         }
       })
     )
-  })
+  });
